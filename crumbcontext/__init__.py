a6abcb3853845a214e444bd7f1169a0121ec3aa3
PenguineWalkOS <slavazeph@gmail.com>
2026-07-12 18:42:38 -0400

    Export routing profiles and schema contracts

--- crumbcontext/__init__.py
+++ crumbcontext/__init__.py
@@ -13,6 +13,13 @@ from .api import (
 from .counterfactual import CounterfactualResult, CounterfactualSpec, run_counterfactual
 from .counterfactual_models import EvaluationSpec
 from .models import Anchor, ContextBlock, Lane, RoutePlan, RoutedBlock
+from .profiles import (
+    ResolvedProfile,
+    available_profiles,
+    custom_profile,
+    resolve_profile,
+    validate_router_config,
+)
 from .providers import (
     AnthropicProvider,
     MockProvider,
@@ -25,11 +32,23 @@ from .providers import (
     get_provider,
 )
 from .router import RouterConfig, route_blocks
+from .schemas import (
+    COUNTERFACTUAL_RESULT_SCHEMA,
+    COUNTERFACTUAL_SPEC_SCHEMA,
+    PROVIDER_REQUEST_SCHEMA,
+    PROVIDER_RESPONSE_SCHEMA,
+    ROUTE_PLAN_SCHEMA,
+    SchemaError,
+    load_json_document,
+    require_schema,
+)
 
 __all__ = [
     "Anchor",
     "AnthropicProvider",
     "BlockInput",
+    "COUNTERFACTUAL_RESULT_SCHEMA",
+    "COUNTERFACTUAL_SPEC_SCHEMA",
     "ContextBlock",
     "CounterfactualResult",
     "CounterfactualSpec",
@@ -37,25 +56,36 @@ __all__ = [
     "Lane",
     "MockProvider",
     "OpenAIProvider",
+    "PROVIDER_REQUEST_SCHEMA",
+    "PROVIDER_RESPONSE_SCHEMA",
     "Provider",
     "ProviderRequest",
     "ProviderResponse",
+    "ROUTE_PLAN_SCHEMA",
+    "ResolvedProfile",
     "RoutePlan",
     "RoutedBlock",
     "RoutedRequestBundle",
     "RouterConfig",
+    "SchemaError",
+    "available_profiles",
     "build_anthropic_payload",
     "build_baseline_request",
     "build_openai_payload",
     "build_routed_request",
     "create_spec",
+    "custom_profile",
     "execute_provider",
     "extract_anchors",
     "get_provider",
+    "load_json_document",
     "normalize_blocks",
+    "require_schema",
+    "resolve_profile",
     "route_blocks",
     "run_counterfactual",
     "sanitize_with_anchors",
+    "validate_router_config",
 ]
 
 __version__ = "0.1.0"
